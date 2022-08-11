56b52b0372e38e25fe1793625224a735865f7ad6
Polina <polinka29enp@gmail.com>
2022-08-11 19:41:19 +0300

    Zadacha10

---
+++ Zadacha10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+//и на выходе показывает вторую цифру этого числа.
+
+
+Console.Write("Введите трехзначное число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int digit = (Number/10)%10;
+Console.WriteLine(digit);
+
+
